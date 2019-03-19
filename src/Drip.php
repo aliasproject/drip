8ba9d68e465fe335d52786dd159547b8f65777f0
Michael Aguiar <mike@aliasproject.com>
2019-03-19 12:54:44 -0700

    drip api

--- src/Drip.php
+++ src/Drip.php
@@ -2,11 +2,9 @@
 
 class Drip
 {
-    const ENDPOINT = 'https://api.getdrip.com/v2/';
-
-    private $account_id;
-    private $api_key;
-    private $headers;
+    protected $endpoint = 'https://api.getdrip.com/v2';
+    private $account_id = null;
+    private $token = false;
 
     /**
      * Create new instance
@@ -16,35 +14,30 @@ class Drip
      * @param string $business_name - Business Name
      * @param string $business_domain - Business Domain
      */
-    public function __construct(string $account_id, string $api_key, string $business_name, string $business_domain)
+    public function __construct(string $account_id, string $token)
     {
         // Set Authentication
         $this->account_id = $account_id;
-        $this->api_key = $api_key;
-
-        // Set Request Headers
-        $this->headers = [
-            'Content-Type: application/json',
-            'User-Agent: ' . $business_name . ' (' . $business_domain . ')'
-        ];
+        $this->token = $token;
     }
 
     /**
      * Add or update subscriber
      *
+     * @param string $email - Subscriber Email
      * @param integer $drip_id - Current user Drip ID for update
      * @param array $custom_fields - Array of custom fields to save to user
      */
-    public function addSubscriber(int $drip_id = null, array $custom_fields = null)
+    public function addSubscriber(string $email, int $drip_id = null, array $custom_fields = null)
     {
         if ($drip_id) {
     		$subscriber_data = [
 				'id' => $drip_id,
-				'new_email' => $_POST['billing_email']
+				'new_email' => $email
 			];
     	} else {
             $subscriber_data = [
-				'email' => $_POST['billing_email']
+				'email' => $email
 			];
     	}
 
@@ -63,15 +56,18 @@ class Drip
      * @param  string  $url
      * @return string
      */
-    private function makeRequest(string $url, array $data = [], bool $post = false)
+    protected function makeRequest(string $url, array $data = [], bool $post = false)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url . '?' . http_build_query($data));
         curl_setopt($ch, CURLOPT_USERPWD, $pwd);
         curl_setopt($ch, CURLOPT_POST, $post);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'AliasProject/Drip (github.com/aliasproject/drip)');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/vnd.api+json',
+            'Content-Type: application/vnd.api+json',
+        ]);
 
         if ($post) {
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
